16784226ef8c3f1c6e84805ecd191c5f655add75
Joie Shepard <me@joieshepard.com>
2013-12-03 14:42:24 -0800

    Added views dir, the new post view html and included the filestystem module

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
-var server = http.createServer(function(request, response) {
+var newPostFormHtml = fs.readFileSync('views/post/new.html');
+
+function renderNewPostForm(request, response){
 	response.writeHead(200, {
-		'Content-type': 'text/plain'
+		'Content-type': 'text/html; charset=utf8'
 	})
-	response.end('Hello World');
+	response.end(newPostFormHtml);
+}
+
+function render404(request, response){
+	response.writeHead('404');
+	response.end('404 File Not Found Bro');
+}
+var server = http.createServer(function(request, response) {
+	var newPostFormRegex = new RegExp('^/posts/new/?$');
+	var pathname = url.parse(request.url).pathname;
+	if(newPostFormRegex.test(pathname))
+	{
+		renderNewPostForm(request, response);
+	} else {
+		render404(request, response);
+	}
+	renderNewPostForm(request, response);
 })
 
 server.listen(8000);
 
-console.log('Listening on http://127:8000');
+console.log('Listening on http://127.0.0.1:8000');
